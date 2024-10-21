1ab01f74517f218c90e729c6a39039bdbe3a5616
Jordyn <48767664+JordynBaldwin@users.noreply.github.com>
2024-10-20 22:38:01 -0700

    changed test screen to tileMap

---
+++ Scripts/coin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_area_entered():
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
