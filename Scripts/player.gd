a058971f0055b4d18984dff0b2f471569e8742b5
Jordyn <48767664+JordynBaldwin@users.noreply.github.com>
2024-10-25 14:56:44 -0700

    improving jump mechanics

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,34 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -8000.0
+const JUMP_VELOCITY = -800.0
+var g_mult = 1.0
+var big_jump = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y < 0 and big_jump:
+			if g_mult <= 1:
+				velocity += get_gravity() * delta * g_mult
+			else:
+				velocity += get_gravity() * delta
+			g_mult = g_mult * 1.5
+		elif velocity.y == 0:
+			velocity.y -= JUMP_VELOCITY * .2
+		elif velocity.y < 900:
+			velocity += get_gravity() * delta * 2.5
+		elif velocity.y > 900:
+			velocity.y = 900
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		g_mult = .3
+		big_jump = true
+	if Input.is_action_just_released("ui_accept"):
+		big_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
