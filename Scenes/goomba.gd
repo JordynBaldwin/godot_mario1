76391665ee8580d8d8d1060bdf842249404d13a4
Monka2e <IDMBaldwin@gmail.com>
2024-10-24 23:21:59 -0700

    goomba can now die and collides with other goombas properly

--- Scenes/goomba.gd
+++ Scenes/goomba.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 70.0
+const SPEED = 40.0
 
-@onready var sprite = $sprite
+@onready var sprite = $AnimatedSprite2D
 @onready var walking_enemy_base = $WalkingEnemyBase
+@onready var death_timer = $DeathTimer
+var dead = false
 
 signal squish
 
@@ -12,15 +14,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func death():
+	dead = true
+	velocity.x = 0
+	walking_enemy_base.queue_free()
 	sprite.play("dead")
+	death_timer.start()
 	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if (walking_enemy_base.direction):
-		velocity.x = walking_enemy_base.direction * SPEED
+	if (!dead):
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		if (walking_enemy_base.direction):
+			velocity.x = walking_enemy_base.direction * SPEED
 
 
 	move_and_slide()
+
+
+func _on_death_timer_timeout():
+	queue_free()
